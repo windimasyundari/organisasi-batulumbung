2e86608c408f7185380ae4d43d18b9a00494bb6a
windimasyundari <windimasyundarii@gmail.com>
2021-12-29 17:54:34 +0800

    initial commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,18 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
-use App\Models\Pengurus;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-// use Auth;
-=======
 use App\Models\Anggota;
 use App\Models\Pengurus;
 use Illuminate\Support\Facades\Hash;
 //use Auth;
 use Illuminate\Support\Facades\Auth;
->>>>>>> a8a1275a0a137a3a019fd10478be4d2d1500dc91
 
 class LoginController extends Controller
 {
@@ -35,23 +28,6 @@ class LoginController extends Controller
      */
     public function prosesLogin(Request $request)
     {
-<<<<<<< HEAD
-        $login = $request->validate([
-            'email' => 'required|email:dns',
-            'password' => 'required'
-        ]);
-
-       if(Auth::attempt($login)) {
-           $request->session()->regenerate();
-
-           return redirect()->intended('pengurus/dashboard');
-       }
-
-       return back()->with('loginError', 'Login gagal!');
-
-    //    dd('berhasil login!');
-=======
-        
         //login anggota
         $emaillogin = Anggota::where('email', $request->email)->first();
         // dd($emaillogin->password);
@@ -87,16 +63,12 @@ class LoginController extends Controller
         {
             return redirect()->back()->with('status', 'Akun tidak terdaftar');
         }
-        //*/
->>>>>>> a8a1275a0a137a3a019fd10478be4d2d1500dc91
+    
     }
     public function dashboardPengurus()
     {
-<<<<<<< HEAD
-        return view('pengurus/dashboard');
-=======
-        //$id = $request->session()->get('idlogin');
-        //$semua = Anggota::where('id', $id)->get(); 
+        $id = $request->session()->get('idlogin');
+        $semua = Anggota::where('id', $id)->get(); 
         return view('/pengurus/dashboard');
 
     }
@@ -144,7 +116,6 @@ class LoginController extends Controller
     public function update(Request $request, $id)
     {
         //
->>>>>>> a8a1275a0a137a3a019fd10478be4d2d1500dc91
     }
 
     public function logout()
